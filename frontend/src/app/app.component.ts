fbc067bcea9abe3e32913f44f5becb4817b2b443
tahlisfove <gdtsamuelchrist@gmail.com>
2025-10-24 03:57:51 +0200

    add dark mode todolist

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, Renderer2, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { TodoFormComponent } from './components/todo-form/todo-form.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
-import { CommonModule } from '@angular/common';
 import { Todo } from './services/todo.service';
 
 @Component({
@@ -11,8 +11,31 @@ import { Todo } from './services/todo.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   @ViewChild(TodoListComponent) todoList!: TodoListComponent;
+
+  isDarkMode = false;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit() {
+    const theme = localStorage.getItem('theme');
+    if (theme === 'dark') {
+      this.isDarkMode = true;
+      this.renderer.addClass(document.body, 'dark-mode');
+    }
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      this.renderer.addClass(document.body, 'dark-mode');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      this.renderer.removeClass(document.body, 'dark-mode');
+      localStorage.setItem('theme', 'light');
+    }
+  }
 
   onTodoAdded(todo: Todo) {
     this.todoList.loadTodos();
