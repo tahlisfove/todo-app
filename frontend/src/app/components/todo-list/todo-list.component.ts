34bbeda4e8bb0e88a80e214faa1d55a4e3d27524
tahlisfove <gdtsamuelchrist@gmail.com>
2025-10-24 04:45:31 +0200

    add drag drop todo and upgrade UI

--- frontend/src/app/components/todo-list/todo-list.component.ts
+++ frontend/src/app/components/todo-list/todo-list.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { TodoService, Todo } from '../../services/todo.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { CdkDragDrop, moveItemInArray, DragDropModule } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, DragDropModule],
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
@@ -20,7 +21,17 @@ export class TodoListComponent implements OnInit {
   }
 
   loadTodos() {
-    this.todoService.getTodos().subscribe(todos => this.todos = todos);
+    this.todoService.getTodos().subscribe(todos => {
+      const savedOrder: number[] = JSON.parse(localStorage.getItem('todosOrder') || '[]');
+      if (savedOrder.length) {
+        this.todos = savedOrder
+          .map((id: number) => todos.find(t => t.id === id))
+          .filter((t): t is Todo => t !== undefined);
+        this.todos.push(...todos.filter(t => !savedOrder.includes(t.id!)));
+      } else {
+        this.todos = todos;
+      }
+    });
   }
 
   updateTodo(todo: Todo) {
@@ -28,6 +39,19 @@ export class TodoListComponent implements OnInit {
   }
 
   deleteTodo(id: number) {
-    this.todoService.deleteTodo(id).subscribe(() => this.loadTodos());
+    this.todoService.deleteTodo(id).subscribe(() => {
+      this.todos = this.todos.filter(t => t.id !== id);
+      this.saveOrder();
+    });
+  }
+
+  drop(event: CdkDragDrop<Todo[]>) {
+    moveItemInArray(this.todos, event.previousIndex, event.currentIndex);
+    this.saveOrder();
+  }
+
+  saveOrder() {
+    const order: number[] = this.todos.map(t => t.id!);
+    localStorage.setItem('todosOrder', JSON.stringify(order));
   }
 }
